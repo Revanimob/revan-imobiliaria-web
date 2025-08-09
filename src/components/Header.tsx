3a372ffb4af8bcc6e6672f0821de9da15deb63ba
Ruan Fonseca <whitelook22@outlook.com>
2025-08-09 13:34:55 -0300

    subindo melhorias de prod

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Home, User } from "lucide-react";
+import { User } from "lucide-react";
 import { Link } from "react-router-dom";
 import logo from "@/assets/Logo Revan P1.png";
 import logodois from "@/assets/Logo Revan P2.png";
@@ -9,10 +9,10 @@ const Header = () => {
   return (
     <header className="bg-white shadow-lg sticky top-0 z-50">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-20 md:h-24 lg:h-21">
           {/* Logo */}
-          <Link to="/" className="flex items-center gap-0">
-            <div className="w-48 h-48 flex items-center justify-center">
+          <Link to="/" className="flex items-center gap-1 md:gap-2">
+            <div className="w-20 h-20 md:w-32 md:h-32 lg:w-48 lg:h-48 flex items-center justify-center">
               <img
                 src={logo}
                 alt="Logo Revan"
@@ -20,7 +20,7 @@ const Header = () => {
               />
             </div>
 
-            <div className="w-48 h-48 flex items-center justify-center">
+            <div className="w-20 h-20 md:w-32 md:h-32 lg:w-48 lg:h-48 flex items-center justify-center">
               <img
                 src={logodois}
                 alt="Logo Revan"
@@ -30,13 +30,13 @@ const Header = () => {
           </Link>
 
           {/* Login Button */}
-          <div className="flex items-center space-x-3">
+          <div className="flex items-center space-x-2 md:space-x-3">
             <Link to="/login">
               <Button
                 variant="outline"
-                className="border-wine text-wine hover:bg-wine hover:text-white"
+                className="border-wine text-wine hover:bg-wine hover:text-white text-sm md:text-base"
               >
-                <User className="w-4 h-4 mr-2" />
+                <User className="w-4 h-4 mr-1 md:mr-2" />
                 Login
               </Button>
             </Link>
